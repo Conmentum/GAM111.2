a531758eb3eeba38091c420580a5c00ccf0ba0b5
Keaghan Potts <kpotts@sae.au>
2017-04-21 16:42:13 +1000

    + added champUI

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+    public GameObject champ_SelectionUI;
+    public GameObject spell_SelectionUI;
+    public int summoner_StonesCount;
+    public Text summoner_stonesText;
+    public Text phaseText;
+
+	// Use this for initialization
+	void Start () {
+        summoner_StonesCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AddCardToDeck()
+    {
+        //onMouseClick set champ to enabled
+    }
+    public enum TurnStates
+    {
+        Start,
+        Draw,
+        Wait,
+        Place,
+        Battle,
+        Place2,
+        End
+    }
+    public TurnStates turnState;
+
+    public void SetTurnState()
+    {
+
+    }
+    void OnEndTurn()
+    {
+
+    }
+    void OnNextPhase()
+    {
+
+    }
+}
